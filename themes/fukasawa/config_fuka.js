e681950cb4e7cb181d38ca777c4d5861e83593f5
jasoncysiu <sjsa3a28@gmail.com>
2024-09-25 12:44:25 +1000

    fe: no archive - testing

--- themes/fukasawa/config_fuka.js
+++ themes/fukasawa/config_fuka.js
@@ -7,7 +7,7 @@ const FUKA_CONFIG = {
   MENU_ABOUT: true, // 显示关于
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: false // 显示搜索
 
 }
--- themes/medium/config_medium.js
+++ themes/medium/config_medium.js
@@ -12,7 +12,7 @@ const CONFIG_MEDIUM = {
   MENU_ABOUT: true, // 显示关于
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
   // Widget
